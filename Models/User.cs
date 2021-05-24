d2f08ff6511ba124eb19e6222517017dd3a23c78
programmer-priya <59862343+programmer-priya@users.noreply.github.com>
2021-05-24 23:11:17 +0530

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCDemoApp.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = " Name is compulsory field")]
+        
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = " Email is compulsory field")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = " MobileNo is compulsory field")]
+        [RegularExpression(@"[0-9]{10}", ErrorMessage = "Enter ten digit number")]
+        public int MobileNo{ get; set; }
+        [Required(ErrorMessage = "Please specify wheather you will attend")]
+        [Display(Name = "WillAttend")]
+        public string WillAttend { get; set; }
+
+
+    }
+
+   
+}
